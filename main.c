7ca274442e9f594188788fa33cb27d02f6319684
ariknoor <arikjahannoor@gmail.com>
2025-12-14 23:02:52 +0600

    Add main.c program

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 int main(){
   printf("CI/CD Pipeline Test Successful\n");
+
+    int a,b;
+    a=7;
+    b=6;
+    int sum = a+b;
+    int mult = a*b;
+    printf("%d\n",sum);
+    printf("%d\n",mult);
+
 return 0;
 }
